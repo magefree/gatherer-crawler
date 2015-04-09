32b415caf5bdad2757b6f5757c05845fe5eba776
LevelX2 <ludwig.hirth@online.de>
2015-04-09 19:57:38 +0200

    Fixed that card lists are read paged now.

--- src/main/java/north/gatherercrawler/Main.java
+++ src/main/java/north/gatherercrawler/Main.java
@@ -1,4 +1,4 @@
-package north.gatherercrawler;
+    package north.gatherercrawler;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -45,17 +45,29 @@ public class Main {
         List<String> sets = new ArrayList<String>();
 
         // Change to false if you only want to add a specific set
-        if (true) {
+        if (false) {
             Document doc = Jsoup.connect("http://gatherer.wizards.com/Pages/Advanced.aspx").get();
             Elements select = doc.select("#autoCompleteSourceBoxsetAddText0_InnerTextBoxcontainer a");
             if (!select.isEmpty()) {
                 for (Element element : select) {
-                    if (!element.text().equals("Unglued") && !element.text().equals("Unhinged")) {
+//                    if (!element.text().equals("Unglued") && !element.text().equals("Unhinged")) {
+                    if (element.text().startsWith("Magic: The Gathering")) {
                         sets.add(element.text());
+                        System.out.println("SetFound: " + element.text());
                     }
                 }
             }
         }
+//        sets.add("Invasion");
+//        sets.add("Magic:+The+Gathering%E2%80%94Conspiracy");
+//        sets.add("Dragons of Tarkir");
+//        sets.add("Duel Decks Anthology");
+        sets.add("Tempest Remastered");
+//        sets.add("Duel Decks: Speed vs. Cunning");
+
+//        sets.add("Starter 2000");
+//        sets.add("Masters Edition III");
+//        sets.add("Masters Edition IV");
 
         readCardsFromFile();
 
@@ -68,8 +80,11 @@ public class Main {
             if (added % 20 == 0 || added == sets.size()) {
                 int retries = 30;
                 boolean done = false;
-                while (retries > 0 && !done) {
-                    String url = "http://gatherer.wizards.com/Pages/Search/Default.aspx?action=advanced&output=checklist&set=" + sb.toString();
+                int page = 0;
+                Integer lastFirstMultiverseId = null;
+                while (retries > 0 && !done) {                    
+                    boolean newIteration = true;
+                    String url = "http://gatherer.wizards.com/Pages/Search/Default.aspx?page=" + page +"&action=advanced&output=checklist&set=" + sb.toString();
                     Connection connection = Jsoup.connect(url);
                     connection.timeout(300000);
                     Document doc = connection.get();
@@ -79,12 +94,22 @@ public class Main {
                     if (!select.isEmpty()) {
                         for (Element element : select) {
                             Integer multiverseId = Integer.parseInt(element.attr("href").replace("../Card/Details.aspx?multiverseid=", ""));
+                            if (newIteration) {
+                                if (lastFirstMultiverseId != null && lastFirstMultiverseId.intValue() == multiverseId) {
+                                    done = true;
+                                    break;
+                                }
+                                lastFirstMultiverseId = multiverseId;
+                                newIteration = false;
+                            }
+                            System.out.println("multiverseId=" + multiverseId);
                             if (!ParsedList.contains(multiverseId)) {
                                 ParseQueue.add(multiverseId);
                             }
                         }
                     }
-                    done = true;
+                    page++;
+                    
                 }
 
                 if (!done) {
